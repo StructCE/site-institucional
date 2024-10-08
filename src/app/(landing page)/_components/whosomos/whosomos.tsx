1e4375f4a110eaee23717fe295052d0aea9f6394
matheusgit <matheusdesouzafernandes54@gmail.com>
2024-10-08 15:30:01 -0300

    sobre nóoos

--- src/app/(landing page)/_components/whosomos/whosomos.tsx
+++ src/app/(landing page)/_components/whosomos/whosomos.tsx
@@ -11,9 +11,9 @@ export default function Whosomos() {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum fringilla imperdiet nibh, eu suscipit mi laoreet eget. Fusce facilisis orci non ante ultricies tincidunt. Pellentesque a lacinia lectus, eu scelerisque velit. Vestibulum elementum augue id feugiat laoreet. Mauris vel metus lacinia, blandit felis in, ultrices nisl.
                     </p>
                     <ul className="flex justify-between">
-                        <li><h1>55</h1><p>servicos</p></li>
-                        <li>no </li>
-                        <li></li>
+                        <li><h1 className="text-center text-5xl font-bold">55</h1><p>Serviços realizados</p></li>
+                        <li><h1 className="text-center text-5xl font-bold">9</h1><p>Anos de história</p></li>
+                        <li><h1 className="text-center text-5xl font-bold">23</h1><p>Membros Ativos</p></li>
                     </ul>
                 </div>
                 <img src="/Praxis - Logo - Simples - Colorida 1.png" alt="imagem" width={513} height={400}/>
