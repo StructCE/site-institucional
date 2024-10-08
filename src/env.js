60029a98e6d07694ae20b8ba477db171970c390c
kalebhenrique <kalebhenriquebr@gmail.com>
2024-10-08 15:41:20 -0300

    Refactor AboutUs component to improve layout and styling

--- src/env.js
+++ src/env.js
@@ -20,10 +20,11 @@ export const env = createEnv({
       // Since NextAuth.js automatically uses the VERCEL_URL if present.
       (str) => process.env.VERCEL_URL ?? str,
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url()
+      process.env.VERCEL ? z.string() : z.string().url(),
     ),
-    DISCORD_CLIENT_ID: z.string(),
-    DISCORD_CLIENT_SECRET: z.string(),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
+    CLOUDINARY_API_SECRET: z.string(),
   },
 
   /**
@@ -33,6 +34,11 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_CLOUDINARY_API_KEY: z.string(),
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: z.string(),
+    NEXT_PUBLIC_MAIL_SERVICE_ID: z.string(),
+    NEXT_PUBLIC_MAIL_TEMPLATE_ID: z.string(),
+    NEXT_PUBLIC_MAIL_USER_ID: z.string(),
   },
 
   /**
@@ -44,8 +50,15 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:
+      process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    NEXT_PUBLIC_CLOUDINARY_API_KEY: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    NEXT_PUBLIC_MAIL_SERVICE_ID: process.env.NEXT_PUBLIC_MAIL_SERVICE_ID,
+    NEXT_PUBLIC_MAIL_TEMPLATE_ID: process.env.NEXT_PUBLIC_MAIL_TEMPLATE_ID,
+    NEXT_PUBLIC_MAIL_USER_ID: process.env.NEXT_PUBLIC_MAIL_USER_ID,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
