796d31d64b44396f44e94d5b9a9011fd5b5ea374
matheusgit <matheusdesouzafernandes54@gmail.com>
2024-11-28 21:02:02 -0300

    Componnente Sobre nós

--- src/app/(landing page)/_components/whosomos/whosomos.tsx
+++ src/app/(landing page)/_components/whosomos/whosomos.tsx
@@ -4,8 +4,8 @@ export default function Whosomos() {
             id="QuemSomos"
             className="mx-8 my-8 flex flex-col lg:mx-36 lg:mb-16 lg:mt-24"
         >
-            <div className="flex flex-col items-center justify-arround md:flex-row">
-                <div className="w-[516px] h-[400px]">
+            <div className="flex flex-col items-center justify-between md:flex-row">
+                <div className=" space-y-4 w-[516px] h-[400px]">
                     <h1 className="mb-4 text-5xl font-bold">Sobre nós</h1>
                     <p className="text-base">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum fringilla imperdiet nibh, eu suscipit mi laoreet eget. Fusce facilisis orci non ante ultricies tincidunt. Pellentesque a lacinia lectus, eu scelerisque velit. Vestibulum elementum augue id feugiat laoreet. Mauris vel metus lacinia, blandit felis in, ultrices nisl.
@@ -16,7 +16,7 @@ export default function Whosomos() {
                         <li><h1 className="text-center text-5xl font-bold">23</h1><p>Membros Ativos</p></li>
                     </ul>
                 </div>
-                <img src="/Praxis - Logo - Simples - Colorida 1.png" alt="imagem" width={513} height={400}/>
+                <img src="default.png" alt="imagem" className="w-[513px] h-[400px]"/>
 
             </div>
         </section> 
